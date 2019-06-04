c3d622bb2795a48a22c7cdf8ce747d4588336259
wufei3623 <1737509789@qq.com>
2019-06-04 16:47:08 +0800

    d

--- app/js/Greeter.js
+++ app/js/Greeter.js
@@ -1,6 +1,7 @@
 // Greeter.js
 var config = require('./config.json');
 
+console.log('dd')
 module.exports = function() {
     let greet = document.createElement('div');
     greet.textContent = config.greetText;
