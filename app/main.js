c2c99cf8c123e181346d64088a2e4f2ee079e770
wufei3623 <1737509789@qq.com>
2019-06-04 16:23:54 +0800

    修改文件路径

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
 //main.js 
-import './main.css';//使用require导入css文件
-const greeter = require('./Greeter.js');
+import './css/main.css';//使用require导入css文件
+const greeter = require('./js/Greeter.js');
 document.querySelector("#root").appendChild(greeter());
