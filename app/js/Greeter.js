a67d880ad79db759e0e536ac844854facd3c05cd
wufei3623 <1737509789@qq.com>
2019-06-04 17:19:16 +0800

    f

--- app/js/Greeter.js
+++ app/js/Greeter.js
@@ -1,9 +1,9 @@
 // Greeter.js
 var config = require('./config.json');
 
-console.log('dd')
-module.exports = function() {
-    let greet = document.createElement('div');
-    greet.textContent = config.greetText;
-    return greet;
-  };
+console.log('测试')
+
+let greet = document.createElement('div');
+greet.textContent = config.greetText;
+
+document.querySelector("#root").appendChild(greet);
--- app/main.js
+++ app/main.js
@@ -1,4 +1,3 @@
 //main.js 
-import './css/main.css';//使用require导入css文件
-const greeter = require('./js/Greeter.js');
-document.querySelector("#root").appendChild(greeter());
+import './css/main.css' 
+import './js/Greeter.js'
